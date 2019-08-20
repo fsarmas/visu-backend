3504cd5bc6febf5dd00ef0defdc649c525c5f49e
Fernando Sanchez <f.sanchez.armas@gmail.com>
2019-08-20 20:41:59 +0200

    Encrypt user passwords before storing in DB (add bcrypt dependency)

--- src/models/User.js
+++ src/models/User.js
@@ -1,11 +1,33 @@
 const mongoose = require('mongoose');
+const config = require('config');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
 
-  email: {type: String, required: true, unique: true},
-  password: String,
+  email: {type: String, required: true, unique: true, trim: true},
+  password: {type: String, required: true, trim: true},
   name: String,
 
 }, {timestamps: true});
+
+/*
+ * Pre-save hook to encrypt password before saving.
+ */
+userSchema.pre('save', function save(next) {
+  const user = this; // eslint-disable-line no-invalid-this
+
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  bcrypt.hash(user.password, config.get('saltingRounds'), (err, hash) => {
+    if (err) {
+      next(err);
+    } else {
+      user.password = hash;
+      next();
+    }
+  });
+});
 
 module.exports = mongoose.model('User', userSchema);

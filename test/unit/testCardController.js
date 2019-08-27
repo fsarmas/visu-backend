2161351b9a46bdb3439070dda56e285c8d78c99a
Fernando Sanchez <f.sanchez.armas@gmail.com>
2019-08-27 22:59:44 +0200

    Rename, document and publish assertEqCards function. Also publish example card data.

--- test/unit/testCardController.js
+++ test/unit/testCardController.js
@@ -12,7 +12,14 @@ const EXAMPLES = [
   {kind: 'rock', name: 'Andalucita'},
 ];
 
-function assertEqCard(card1, card2) {
+/**
+ * Assert that the given objects have the same 'kind' and 'name' fields,
+ * throwing an exception otherwise.
+ *
+ * @param   {!object} card1 the first card to compare
+ * @param   {!object} card2 the second card to compare
+ */
+function assertEqCards(card1, card2) {
   assert.strictEqual(card1.kind, card2.kind);
   assert.strictEqual(card1.name, card2.name);
 }
@@ -29,10 +36,10 @@ describe('cardController tests', function() {
 
   it('Create card', async function() {
     const created0 = await cardController.create(EXAMPLES[0]);
-    assertEqCard(EXAMPLES[0], created0);
+    assertEqCards(EXAMPLES[0], created0);
 
     const created1 = await cardController.create(EXAMPLES[1]);
-    assertEqCard(EXAMPLES[1], created1);
+    assertEqCards(EXAMPLES[1], created1);
   });
 
   it('List with skip and limit', async function() {
@@ -43,26 +50,26 @@ describe('cardController tests', function() {
     const result0 = await cardController.list(0, 3);
     assert.lengthOf(result0, 3);
     for (let i = 0; i < 3; i++) {
-      assertEqCard(EXAMPLES[i], result0[i]);
+      assertEqCards(EXAMPLES[i], result0[i]);
     }
 
     const result1 = await cardController.list(2, 2);
     assert.lengthOf(result1, 2);
     for (let i = 0; i < 2; i++) {
-      assertEqCard(EXAMPLES[i+2], result1[i]);
+      assertEqCards(EXAMPLES[i+2], result1[i]);
     }
 
     const result2 = await cardController.list(4, 4);
     assert.lengthOf(result2, 2);
     for (let i = 0; i < 2; i++) {
-      assertEqCard(EXAMPLES[i+4], result2[i]);
+      assertEqCards(EXAMPLES[i+4], result2[i]);
     }
   });
 
   it('Find by id', async function() {
     const created = await cardController.create(EXAMPLES[0]);
     const found = await cardController.findById(created._id);
-    assertEqCard(created, found);
+    assertEqCards(created, found);
 
     const notFound = await cardController.findById(
         new mongoose.Types.ObjectId());
@@ -72,7 +79,7 @@ describe('cardController tests', function() {
   it('Update', async function() {
     const created = await cardController.create(EXAMPLES[0]);
     const updated = await cardController.update(created._id, EXAMPLES[1]);
-    assertEqCard(updated, EXAMPLES[1]);
+    assertEqCards(updated, EXAMPLES[1]);
     assert.exists(updated.id);
     assert.isOk(created._id.equals(updated._id));
   });
@@ -90,7 +97,7 @@ describe('cardController tests', function() {
     assert.lengthOf(result0, EXAMPLES.length);
 
     const deleted = await cardController.delete(id);
-    assertEqCard(deleted, EXAMPLES[0]);
+    assertEqCards(deleted, EXAMPLES[0]);
     assert.isOk(id.equals(deleted._id));
 
     const result1 = await cardController.list();
@@ -101,3 +108,6 @@ describe('cardController tests', function() {
     assert.deepEqual(result3, []);
   });
 });
+
+module.exports.EXAMPLES = EXAMPLES;
+module.exports.assertEqCards = assertEqCards;
